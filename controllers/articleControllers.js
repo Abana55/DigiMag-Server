412208fc9b25e15c6fde720cabe7b64d60546d53
Alec bana <anbana994@gmail.com>
2024-01-13 23:15:03 -0500

    added creation of articles

--- controllers/articleControllers.js
+++ controllers/articleControllers.js
@@ -6,3 +6,14 @@ const getArticles = async (req, res) => {
         res.status(500).json({ message: error.message })
     }
 };
+
+const createArticle = sync (req, res) => {
+    try {
+        const {title, content, author} = req.body;
+        const newArticle = new Article({title, content, author});
+        await newArticle.save();
+        res.status(201).json(newArticle);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
