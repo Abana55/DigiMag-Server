0befb28e1fd4b5dfada102df01a5be2dc2dfcd5d
Alec bana <anbana994@gmail.com>
2024-01-13 23:23:57 -0500

    added update to the aarticle

--- controllers/articleControllers.js
+++ controllers/articleControllers.js
@@ -17,3 +17,19 @@ const createArticle = sync (req, res) => {
         res.status(400).json({ message: error.message });
     }
 }
+
+const updateArticle = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updatedArticle = await Article.findByIdAndUpdate(id, req.body, { new: true });
+        if (!updatedArticle) {
+            return res.status(404).json({ message: "Article not found" });
+        }
+        res.json(updatedArticle);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+router.post('/', createArticle);
+
